fc608a302b5f923092c0dc43ca2c66d8227f4dd1
rtemplo <30515414+rtemplo@users.noreply.github.com>
2026-05-12 12:27:34 -0400

    cleanup

--- src/filtering/dedupe.ts
+++ src/filtering/dedupe.ts
@@ -15,21 +15,4 @@ function splitUniqueAndDuplicates(nums: number[]): { uniques: number[]; duplicat
 
 // Example
 const arr: number[] = [4, 7, 4, 2, 9, 2, 2, 5];
-// console.log(splitUniqueAndDuplicates(arr));
-
-const sortUniquesVsDupes = (numbers: number[]): { unique: number[]; dupes: number[] } => {
-  const counts = new Map<number, number>();
-  for (const n of numbers) counts.set(n, (counts.get(n) ?? 0) + 1);
-
-  const unique: number[] = [];
-  const dupes: number[] = [];
-
-  for (const [n, c] of counts) {
-    if (c === 1) unique.push(n);
-    else dupes.push(n);
-  }
-
-  return { unique, dupes };
-};
-
-console.log(sortUniquesVsDupes(arr));
+console.log(splitUniqueAndDuplicates(arr));
